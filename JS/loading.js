930b0563d961e46227cabed625c1ca59bb8a365c
Jumpei <jumpei.yamada.nagoya@gmail.com>
2023-12-30 01:51:52 +0900

    loadingで脱jQueryした

--- JS/loading.js
+++ JS/loading.js
@@ -1,14 +1,27 @@
-const loading = document.getElementById("loading");
-const randomList = ["/img/loading-min.GIF", "/img/loading2.gif"];
-const num = Math.floor(Math.random() * randomList.length);
-const printHtml = `<img src="${randomList[num]}" alt="ランダム画像">`;
-loading.innerHTML = printHtml;
+{
+  const loading = document.getElementById("loading");
+  const randomList = ["/img/loading-min.GIF", "/img/loading2.gif"];
+  const num = Math.floor(Math.random() * randomList.length);
+  const printHtml = `<img src="${randomList[num]}" id="loadingImg" alt="ランダム画像">`;
+  loading.innerHTML = printHtml;
 
-window.addEventListener("load", stopload);
-setTimeout(stopload, 10000);
+  window.addEventListener("load", stopload);
+  setTimeout(stopload, 10000);
 
-function stopload() {
-  // imgがフェードアウトした後に#loadingがフェードアウトする
-  $("#loading").delay(1500).fadeOut(800);
-  $("#loading img").delay(1200).fadeOut(300);
+  function stopload() {
+    const loadingImg = document.getElementById("loadingImg");
+
+    new Promise((resolve) => {
+      // 1.2秒スリープして、#loadingImgがフェードアウト
+      setTimeout(() => {
+        loadingImg.classList.add("loaded");
+        resolve();
+      }, 1200);
+    }).then(() => {
+      // #loadingImgのフェードアウトを待ってから#loadingがフェードアウト
+      setTimeout(() => {
+        loading.classList.add("loaded");
+      }, 300);
+    });
+  }
 }
